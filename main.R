6ebb675faf64ff5d89115edb3b2426fe01a0b25e
BartekPog <bartek.pogod@gmail.com>
2021-03-10 18:42:06 +0100

    Add box-and-whiskers plot generation

--- main.R
+++ main.R
@@ -2,6 +2,7 @@ library(tidyverse) # includes many useful data analysis tools
 library(ggplot2)
 
 source("src/data-cleaning.R")
+source("src/plots/box-and-whiskers.R")
 
 DATA_FILE <- "data/heart.csv"
 
@@ -12,7 +13,7 @@ data <- read.csv(DATA_FILE)
 # Data cleaning
 dataClean <- cleanData(data)
 
-
 # Graph generation
+generateBoxAndWhiskers(dataClean)
 
 # Hyphotheses testing

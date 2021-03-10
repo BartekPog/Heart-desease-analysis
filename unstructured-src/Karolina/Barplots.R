110576bac87ed707c90c2a04d1c90840dcce7b25
Karolina <lukierek82@wp.pl>
2021-03-10 15:48:58 +0100

    Barplots

--- unstructured-src/Karolina/Barplots.R
+++ unstructured-src/Karolina/Barplots.R
@@ -19,13 +19,30 @@ arg6 <- table(fbs)
 barplot(arg6, col = rainbow(20), xlab = "Poziom cukru we krwi na czczo 120 mg/dL (1 = wykryto, 0 = nie wykryto)", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od ich poziomu cukru we krwi na czczo")
 
 arg7 <- table(restecg)
-barplot(arg7, col = rainbow(20), xlab = "Wykonanie testu EKG (1 = wykonano, 0 = nie wykonano)", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od wykonania testu EKG")
+barplot(arg7, col = rainbow(20), xlab = "Wynik testu EKG (Wartości 0, 1, 2)", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od wykonania testu EKG")
 
 arg8 <- table(thalach)
 barplot(arg8, col = rainbow(20), xlab = "Maksymalne tętno", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od wykrytego maksymalnego tętna")
 
 arg9 <- table(exang)
 barplot(arg9, col = rainbow(20), xlab = "Wykrycie dławicy wysiłkowej (1 = wykryto, 0 = nie wykryto)", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od wykrytej dławicy wysiłkowej")
+
+arg12 <- table(oldpeak)
+barplot(arg12, col = rainbow(20), xlab = "Segment ST", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od segmentu ST")
+
+arg13 <- table(slope)
+barplot(arg13, col = rainbow(20), xlab = "Nachylenie szczytowego odcinka ST wysiłkowego.", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od nachylenia szczytowego odcinka ST wysiłkowego")
+
+arg14 <- table(ca)
+barplot(arg14, col = rainbow(20), xlab = "Liczba zabarwionych głównych naczynek (0-3)", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od liczby zabarwionych głównych naczynek")
+
+arg15 <- table(thal)
+barplot(arg15, col = rainbow(20), xlab = "Thal: 3 = normalny; 6 = naprawiona wada; 7 = odwracalna wada", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od thal")
+
+arg16 <- table(target)
+barplot(arg16, col = rainbow(20), xlab = "1 = obecność choroby serca, 0 = brak obecności choroby serca", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od występowania choroby serca")
+
+
 
 arg10 <- table(sex,age)
 barplot(arg10, col = rainbow(2), xlab = "Wiek", ylab = "Liczba badanych", main = "Wykres zależności liczby badanych osób od wieku i płci", legend("topright", c("Kobieta", "Mężczyzna"), cex = 1, bty = "n", fill = rainbow(2)))

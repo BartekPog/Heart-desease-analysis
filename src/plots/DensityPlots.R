05f8146936c9b9c640804f7b66016d53b520788b
DawidCieslik <dawicie210@student.polsl.pl>
2021-03-13 00:41:21 +0100

    Update DensityPlots.R

--- src/plots/DensityPlots.R
+++ src/plots/DensityPlots.R
@@ -5,154 +5,85 @@ generateDensityPlots <- function(data)
   ggplot(data,aes(x=age)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci wieku") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Wiek")
-  
+    labs(x="Wiek", y="Gêstoœæ")
   ggsave("images/density-age.png")
-  
   
   ggplot(data,aes(x=sex)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci p³ci") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="P³eæ")
-
+    labs(x="P³eæ", y="Gêstoœæ")
   ggsave("images/density-sex.png")
-  
   
   ggplot(data,aes(x=cp)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci typu bólu klatki piersiowej") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Typ bólu klatki piersiowej")
-  
+    labs(x="Typ bólu klatki piersiowej", y="Gêstoœæ")
   ggsave("images/density-cp.png")
-  
   
   ggplot(data,aes(x=trestbps)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci ciœnienia spoczynkowego") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Ciœnienie spoczynkowe")
-  
+    labs(x="Ciœnienie spoczynkowe", y="Gêstoœæ")
   ggsave("images/density-trestbps.png")
-  
   
   ggplot(data,aes(x=chol)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci poziomu cholesterolu") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Poziom cholesterolu")
-  
+    labs(x="Poziom cholesterolu", y="Gêstoœæ")
   ggsave("images/density-chol.png")
-  
   
   ggplot(data,aes(x=fbs)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci poziomu cukru we krwi na czczo") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Poziom cukru we krwi na czczo")
-  
+    labs(x="Poziom cukru we krwi na czczo", y="Gêstoœæ")
   ggsave("images/density-fbs.png")
-  
   
   ggplot(data,aes(x=restecg)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci spoczynkowych wyników elektrokardiograficznych") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Spoczynkowe wyniki EKG")
-  
+    labs(x="Spoczynkowe wyniki EKG", y="Gêstoœæ")
   ggsave("images/density-restecg.png")
-  
   
   ggplot(data,aes(x=thalach)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci maksymalnego zarejestrowanego têtna") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Maksymalne zarejestrowane têtno")
-  
+    labs(x="Maksymalne zarejestrowane têtno", y="Gêstoœæ")
   ggsave("images/density-thalach.png")
-  
   
   ggplot(data,aes(x=exang)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci wykrytej d³awicy wysi³kowej") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Wykryta d³awica wysi³kowa")
-  
+    labs(x="Wykryta d³awica wysi³kowa", y="Gêstoœæ")
   ggsave("images/density-exang.png")
-  
 
   ggplot(data,aes(x=oldpeak)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci wzglêdnego obni¿enia rejonu ST w w czasie aktywnoœci fizycznej") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Wzglêdne obni¿enie rejonu ST w w czasie aktywnoœci fizycznej")
-  
+    labs(x="Wzglêdne obni¿enie rejonu ST w w czasie aktywnoœci fizycznej", y="Gêstoœæ")
   ggsave("images/density-oldpeak.png")
-  
   
   ggplot(data,aes(x=slope)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci nachylenia szczytowego odcinka ST wysi³kowego") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Nachylenie szczytowe odcinka ST wysi³kowego")
-  
+    labs(x="Nachylenie szczytowe odcinka ST wysi³kowego", y="Gêstoœæ")
   ggsave("images/density-slope.png")
-  
   
   ggplot(data,aes(x=ca)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci liczby zabarwionych g³ównych naczyñ") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Liczba zabarwionych g³ównych naczyñ")
-  
+    labs(x="Liczba zabarwionych g³ównych naczyñ", y="Gêstoœæ")
   ggsave("images/density-ca.png")
-  
   
   ggplot(data,aes(x=thal)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci talu") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Tal")
-  
+    labs(x="Tal", y="Gêstoœæ")
   ggsave("images/density-thal.png")
-  
   
   ggplot(data,aes(x=target)) +
     geom_density(fill="steelblue")+
     ggtitle("Wykres gêstoœci wystêpowania chorób serca") + 
-    theme(axis.title.y=element_blank(),
-          axis.text.y=element_blank(),
-          axis.ticks.y=element_blank()) +
-    labs(x="Wystêpowanie chorób serca")
-  
+    labs(x="Wystêpowanie chorób serca", y="Gêstoœæ")
   ggsave("images/density-target.png")
   
   return()

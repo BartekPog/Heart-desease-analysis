f2ee3de8fccfb6f935a27131d9d69671c0354bdb
DawidCieslik <dawicie210@student.polsl.pl>
2021-03-13 10:21:05 +0100

    Update main.R

--- main.R
+++ main.R
@@ -3,6 +3,12 @@ library(ggplot2)
 
 source("src/data-cleaning.R")
 source("src/plots/box-and-whiskers.R")
+<<<<<<< Updated upstream
+=======
+source("src/plots/Scatterplot-Matrix.R")
+source("src/plots/2D-density.R")
+source("src/plots/DensityPlots.R")
+>>>>>>> Stashed changes
 
 DATA_FILE <- "data/heart.csv"
 
@@ -12,6 +18,9 @@ data <- read.csv(DATA_FILE)
 
 # Data cleaning
 dataClean <- cleanData(data)
+
+## Density Plots
+generateDensityPlots(dataClean)
 
 # Graph generation
 generateBoxAndWhiskers(dataClean)

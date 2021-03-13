0b1656774b757933e4943af4570612a985c0d92d
annawolll <56248052+annawolll@users.noreply.github.com>
2021-03-13 09:11:34 +0100

    Scatterplot Matrix

--- main.R
+++ main.R
@@ -1,8 +1,10 @@
 library(tidyverse) # includes many useful data analysis tools
 library(ggplot2)
+library(GGally)
 
 source("src/data-cleaning.R")
 source("src/plots/box-and-whiskers.R")
+source("src/plots/Scatterplot-Matrix.R")
 
 DATA_FILE <- "data/heart.csv"
 
@@ -18,5 +20,8 @@ generateBoxAndWhiskers(dataClean)
 
 ## 2D - density plots
 generate2DDensity(dataClean)
+
+#Scatterplot Matrix generation
+generateScatterPlot(dataClean)
 
 # Hyphotheses testing

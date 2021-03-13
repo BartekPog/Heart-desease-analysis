f800927e60e6760e53d96aea2cc3cd7291ba7d73
DawidCieslik <56017629+DawidCieslik@users.noreply.github.com>
2021-03-13 10:32:17 +0100

    Update main.R

--- main.R
+++ main.R
@@ -1,14 +1,13 @@
 library(tidyverse) # includes many useful data analysis tools
 library(ggplot2)
+library(GGally)
 
 source("src/data-cleaning.R")
 source("src/plots/box-and-whiskers.R")
-<<<<<<< Updated upstream
-=======
 source("src/plots/Scatterplot-Matrix.R")
 source("src/plots/2D-density.R")
+source("src/plots/BarPlots.R")
 source("src/plots/DensityPlots.R")
->>>>>>> Stashed changes
 
 DATA_FILE <- "data/heart.csv"
 
@@ -19,10 +18,19 @@ data <- read.csv(DATA_FILE)
 # Data cleaning
 dataClean <- cleanData(data)
 
-## Density Plots
+# Density Plots
 generateDensityPlots(dataClean)
 
 # Graph generation
 generateBoxAndWhiskers(dataClean)
+
+## 2D - density plots
+generate2DDensity(dataClean)
+
+#Scatterplot Matrix generation
+generateScatterPlot(dataClean)
+
+# Barplots generation
+generateBarPlots(dataClean)
 
 # Hyphotheses testing

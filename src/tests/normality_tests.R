44b59e16f689defc72d41b10631fc9dce9dd4bd5
DawidCieslik <dawicie210@student.polsl.pl>
2021-03-22 21:06:38 +0100

    Update normality_tests.R

--- src/tests/normality_tests.R
+++ src/tests/normality_tests.R
@@ -1,10 +1,20 @@
+library(ggpubr)
+library(ggplot2)
+
 NormalityTests <- function(data)
 {
+  ggqqplot(data$age)
+  ggsave("images/age_QQ_plot.png")
+  ggqqplot(data$chol)
+  ggsave("images/chol_QQ_plot.png")
+  ggqqplot(data$trestbps)
+  ggsave("images/trestbps_QQ_plot.png")
+  ggqqplot(data$thalach)
+  ggsave("images/thalach_QQ_plot.png")
   sink(file = "tests/Shapiro-Wilk-normality-tests.txt")
   shapiro.test(data$age)
   shapiro.test(data$chol)
   shapiro.test(data$trestbps)
   shapiro.test(data$thalach)
   sink()
-  unlink("tests/Shapiro-Wilk-normality-tests.txt")
 }

c40e822e06acd4d5dbeca9560d564081a19cb492
Patrick <33777820+PSb19@users.noreply.github.com>
2021-03-29 19:08:16 +0200

    modification of .txt generation (#12)
    * chi-square for discrete
    
    * correct chi-squared test
    
    * added text output
    
    * .txt hotfix

--- src/tests/chi-square-discrete.R
+++ src/tests/chi-square-discrete.R
@@ -2,6 +2,10 @@
 
 generateChiSqTest<-function(data){
   sink(file="images/chiSq-cp-diag.txt")
+    ## Test 1 - raw data:
+    chisq.test(data$cp, data$diag, correct = FALSE, simulate.p.value = FALSE)
+
+    ##Test 2 - simulation:
     chisq.test(data$cp, data$diag, correct = FALSE, simulate.p.value = TRUE)
     ## We use simulate.p.value, because of small count of elements
     ## df = NA because we simulate values.
